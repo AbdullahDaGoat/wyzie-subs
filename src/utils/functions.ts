837705b2f8215c5b910c3bbf3bb99264685bcdbc
Abdullah <abdullahaviator13@gmail.com>
2024-06-24 14:13:40 -0400

    Made a few minor changes need to talk to Baddev before doing anything

--- src/utils/functions.ts
+++ src/utils/functions.ts
@@ -1,5 +1,3 @@
-import { ResponseType, RequestType } from "./types";
-
 export async function search(request: RequestType, language?: string): Promise<ResponseType[]> {
   const data = await fetchSubtitles(request);
 
